53445f22aac814f9338b36e9d90d77db0ba18453
darklulu <luk0607@gmail.com>
2021-11-07 18:07:24 +0100

    build v3

--- assets/script/style.js
+++ assets/script/style.js
@@ -16,3 +16,18 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+
+function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("img-carou").src = images[x];
+}
+function startTimer() {
+    setInterval(displayNextImage, 500);
+}
+var images = [], x = -1;
+images[0] = "/assets/images/56.png";
+images[1] = "/assets/images/65.png";
+images[2] = "/assets/images/91.png";
+images[3] = "/assets/images/97.png";
+    
